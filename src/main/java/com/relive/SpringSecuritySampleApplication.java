2eaa236cd18190398795ec25db14e2e0b7ba58ee
ReLive27 <re_live27@163.com>
2022-11-29 22:50:50 +0800

    Init project

---
+++ src/main/java/com/relive/SpringSecuritySampleApplication.java
@@ -0,0 +1,13 @@
+package com.relive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecuritySampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecuritySampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/relive/SpringSecuritySampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.relive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecuritySampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
