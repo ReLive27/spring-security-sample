d57596d54a9fdf26fb1a9d8b0e403722445c108b
ReLive27 <re_live27@163.com>
2022-11-30 21:36:43 +0800

    Init project

--- cas-login/src/main/java/com/relive/CasLoginApplication.java
+++ cas-login/src/main/java/com/relive/CasLoginApplication.java
@@ -3,11 +3,14 @@ package com.relive;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author: ReLive
+ * @date: 2022/11/30 19:45
+ */
 @SpringBootApplication
-public class SpringSecuritySampleApplication {
+public class CasLoginApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringSecuritySampleApplication.class, args);
+        SpringApplication.run(CasLoginApplication.class, args);
     }
-
 }
