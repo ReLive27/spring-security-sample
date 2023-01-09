a81d9a9ad7d9f7cc814eb4200d711057406b56f8
ReLive27 <re_live27@163.com>
2023-01-09 21:07:31 +0800

    Update MfaUserDdetails

--- mfa-login/src/main/java/com/relive/mfa/userdetails/MfaUserDetails.java
+++ mfa-login/src/main/java/com/relive/mfa/userdetails/MfaUserDetails.java
@@ -3,65 +3,90 @@ package com.relive.mfa.userdetails;
 import org.springframework.security.core.CredentialsContainer;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.util.Assert;
 
 import java.util.Collection;
+import java.util.Collections;
+import java.util.Set;
+import java.util.TreeSet;
 
 /**
  * @author: ReLive
  * @date: 2023/1/8 22:40
  */
 public class MfaUserDetails implements UserDetails, CredentialsContainer {
-
-    //TODO 待补充
-
-    private boolean mfa;
+    private final String username;
+    private String password;
+    private final Set<GrantedAuthority> authorities;
+    private final boolean accountNonExpired;
+    private final boolean accountNonLocked;
+    private final boolean credentialsNonExpired;
+    private final boolean enabled;
+    private boolean enableMfa;
     private String secret;
+
+    public MfaUserDetails(String username, String password, boolean enableMfa, String secret, Collection<? extends GrantedAuthority> authorities) {
+        this(username, password, enableMfa, secret, true, true, true, true, authorities);
+    }
+
+    public MfaUserDetails(String username, String password, boolean enableMfa, String secret, boolean enabled, boolean accountNonExpired, boolean credentialsNonExpired, boolean accountNonLocked, Collection<? extends GrantedAuthority> authorities) {
+        Assert.isTrue(username != null && !"".equals(username) && password != null, "Cannot pass null or empty values to constructor");
+        this.username = username;
+        this.password = password;
+        this.enabled = enabled;
+        this.accountNonExpired = accountNonExpired;
+        this.credentialsNonExpired = credentialsNonExpired;
+        this.accountNonLocked = accountNonLocked;
+        this.authorities = Collections.unmodifiableSet(new TreeSet(authorities));
+        this.enableMfa = enableMfa;
+        this.secret = secret;
+    }
 
     @Override
     public void eraseCredentials() {
-
+        this.password = null;
     }
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return null;
+        return this.authorities;
     }
 
     @Override
     public String getPassword() {
-        return null;
+        return this.password;
     }
 
     @Override
     public String getUsername() {
-        return null;
+        return this.username;
     }
 
     @Override
     public boolean isAccountNonExpired() {
-        return false;
+        return this.accountNonExpired;
     }
 
     @Override
     public boolean isAccountNonLocked() {
-        return false;
+        return this.accountNonLocked;
     }
 
     @Override
     public boolean isCredentialsNonExpired() {
-        return false;
+        return this.credentialsNonExpired;
     }
 
     @Override
     public boolean isEnabled() {
-        return false;
+        return this.enabled;
     }
 
     public String getSecret() {
-        return secret;
+        return this.secret;
     }
 
-    public boolean isMfa() {
-        return mfa;
+    public boolean isEnableMfa() {
+        return enableMfa;
     }
 }
