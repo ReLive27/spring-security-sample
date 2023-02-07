1c73e4f05ccc42b679bdc5d46caab169cdec028a
ReLive27 <re_live27@163.com>
2023-02-07 20:02:23 +0800

    Init project

---
+++ prevent-brute-force/src/main/java/com/relive/PreventBruteForceApplication.java
@@ -0,0 +1,16 @@
+package com.relive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author: ReLive
+ * @date: 2023/2/7 19:59
+ */
+@SpringBootApplication
+public class PreventBruteForceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PreventBruteForceApplication.class, args);
+    }
+}
