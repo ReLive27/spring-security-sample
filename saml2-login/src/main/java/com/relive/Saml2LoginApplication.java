3c3be213af721461a20df9d2b4faacac7e673825
ReLive27 <re_live27@163.com>
2023-02-20 20:20:27 +0800

    Enable saml2-login module

---
+++ saml2-login/src/main/java/com/relive/Saml2LoginApplication.java
@@ -0,0 +1,16 @@
+package com.relive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author: ReLive
+ * @date: 2023/2/20 20:19
+ */
+@SpringBootApplication
+public class Saml2LoginApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Saml2LoginApplication.class, args);
+    }
+}
