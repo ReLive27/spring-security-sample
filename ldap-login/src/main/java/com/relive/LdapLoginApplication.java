995930ed16e3796c20691345f7d2f5a85c854ca2
ReLive27 <re_live27@163.com>
2023-02-20 20:11:37 +0800

    Add LdapLoginApplication

---
+++ ldap-login/src/main/java/com/relive/LdapLoginApplication.java
@@ -0,0 +1,16 @@
+package com.relive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author: ReLive
+ * @date: 2023/2/20 20:10
+ */
+@SpringBootApplication
+public class LdapLoginApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LdapLoginApplication.class, args);
+    }
+}
