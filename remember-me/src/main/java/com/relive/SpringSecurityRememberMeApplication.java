ed605cba7412b28f2473cbc1ae38a17975da83ba
ReLive27 <re_live27@163.com>
2023-02-23 20:38:22 +0800

    Add SpringSecurityRememberMeApplication.java

---
+++ remember-me/src/main/java/com/relive/SpringSecurityRememberMeApplication.java
@@ -0,0 +1,16 @@
+package com.relive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author: ReLive
+ * @date: 2023/2/21 19:51
+ */
+@SpringBootApplication
+public class SpringSecurityRememberMeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecurityRememberMeApplication.class, args);
+    }
+}
