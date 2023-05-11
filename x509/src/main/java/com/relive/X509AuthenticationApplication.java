d0ab738739dd860910e9bc6754349ffd641e4fd3
ReLive27 <re_live27@163.com>
2023-05-11 11:02:02 +0800

    Enable x509 module

---
+++ x509/src/main/java/com/relive/X509AuthenticationApplication.java
@@ -0,0 +1,16 @@
+package com.relive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author: ReLive
+ * @date: 2023/5/10 19:34
+ */
+@SpringBootApplication
+public class X509AuthenticationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(X509AuthenticationApplication.class, args);
+    }
+}
