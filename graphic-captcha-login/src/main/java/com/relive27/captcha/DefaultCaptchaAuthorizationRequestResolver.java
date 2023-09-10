1891dff0eb88e4c70651dc10a7d62056719c6d38
ReLive27 <re_live27@163.com>
2023-09-10 20:35:40 +0800

    Add post request body parameter parsing.

--- graphic-captcha-login/src/main/java/com/relive27/captcha/DefaultCaptchaAuthorizationRequestResolver.java
+++ graphic-captcha-login/src/main/java/com/relive27/captcha/DefaultCaptchaAuthorizationRequestResolver.java
@@ -1,5 +1,8 @@
 package com.relive27.captcha;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.json.JsonParser;
+import org.springframework.boot.json.JsonParserFactory;
 import org.springframework.http.HttpMethod;
 import org.springframework.lang.Nullable;
 import org.springframework.security.core.Authentication;
@@ -8,11 +11,13 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import javax.servlet.http.HttpServletRequest;
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.util.Map;
 
 /**
  * @author: ReLive27
  * @date: 2023/8/31 17:33
  */
+@Slf4j
 public class DefaultCaptchaAuthorizationRequestResolver implements CaptchaAuthorizationRequestResolver {
     public static final String SPRING_SECURITY_FORM_USERNAME_KEY = "username";
     public static final String SPRING_SECURITY_FORM_CAPTCHA_KEY = "captcha";
@@ -21,15 +26,32 @@ public class DefaultCaptchaAuthorizationRequestResolver implements CaptchaAuthor
 
     private String captchaParameter = SPRING_SECURITY_FORM_CAPTCHA_KEY;
 
+    private final JsonParser jsonParser = JsonParserFactory.getJsonParser();
+
     @Override
     public CaptchaAuthorizationRequest resolve(HttpServletRequest request) throws IOException {
         CaptchaAuthorizationRequest.Builder builder = CaptchaAuthorizationRequest.host(getIpAddr(request));
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         if (HttpMethod.POST.equals(HttpMethod.resolve(request.getMethod()))) {
             try (BufferedReader reader = request.getReader()) {
-                //TODO 读取请求体中验证码信息
-            } catch (IOException e) {
+                String tmp;
+                StringBuilder body = new StringBuilder();
+                while ((tmp = reader.readLine()) != null) {
+                    body.append(tmp);
+                }
 
+                Map<String, Object> bodyMap = jsonParser.parseMap(body.toString());
+                String captcha = (String) bodyMap.get(captchaParameter);
+                builder.captcha(captcha);
+                if (authentication == null) {
+                    String username = (String) bodyMap.get(usernameParameter);
+                    builder.username(username);
+                } else {
+                    builder.username(authentication.getName());
+                }
+            } catch (IOException e) {
+                log.error("Failed to parse verification code request body", e);
+                return null;
             }
         } else {
             builder.captcha(this.obtainCaptcha(request));
@@ -61,5 +83,13 @@ public class DefaultCaptchaAuthorizationRequestResolver implements CaptchaAuthor
         } else {
             return xfHeader.split(",")[0];
         }
+    }
+
+    public void setUsernameParameter(String usernameParameter) {
+        this.usernameParameter = usernameParameter;
+    }
+
+    public void setCaptchaParameter(String captchaParameter) {
+        this.captchaParameter = captchaParameter;
     }
 }
