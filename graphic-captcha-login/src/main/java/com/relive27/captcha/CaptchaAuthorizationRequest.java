763bd3322c6d019d3c30efcf9d9622a74af18854
ReLive27 <re_live27@163.com>
2023-09-10 20:13:11 +0800

    Delete host verification

--- graphic-captcha-login/src/main/java/com/relive27/captcha/CaptchaAuthorizationRequest.java
+++ graphic-captcha-login/src/main/java/com/relive27/captcha/CaptchaAuthorizationRequest.java
@@ -4,9 +4,6 @@ import lombok.Data;
 import org.springframework.util.Assert;
 import org.springframework.util.StringUtils;
 
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
 /**
  * @author: ReLive27
  * @date: 2023/8/29 14:38
@@ -53,22 +50,13 @@ public class CaptchaAuthorizationRequest {
         }
 
         public CaptchaAuthorizationRequest build() {
-            if (!StringUtils.hasText(this.captcha)) {
-                throw new BadCaptchaException("验证码不能为空");
-            }
+            Assert.hasText(this.captcha, "captcha cannot be empty");
             Assert.hasText(this.host, "host cannot be empty");
-            Assert.isTrue(this.checkHost(), "host is invalid");
             CaptchaAuthorizationRequest authorizationRequest = new CaptchaAuthorizationRequest();
             authorizationRequest.captcha = this.captcha;
             authorizationRequest.host = this.host;
             authorizationRequest.username = this.username;
             return authorizationRequest;
-        }
-
-        private boolean checkHost() {
-            Pattern pattern = Pattern.compile("^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$");
-            Matcher matcher = pattern.matcher(this.host);
-            return matcher.matches();
         }
     }
 }
