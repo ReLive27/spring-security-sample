4b94ebc591ebd04cf0f7a096c20129d48c7a51cf
ReLive27 <re_live27@163.com>
2023-09-11 08:49:03 +0800

    Add SecurityConfig.java

---
+++ graphic-captcha-login/src/main/java/com/relive27/SecurityConfig.java
@@ -0,0 +1,58 @@
+package com.relive27;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.relive27.captcha.CaptchaAuthenticationConfigurer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.AuthenticationEntryPointFailureHandler;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
+
+import java.io.Writer;
+import java.util.Collections;
+import java.util.UUID;
+
+/**
+ * @author: ReLive27
+ * @date: 2023/8/28 11:12
+ */
+@Configuration(proxyBeanMethods = false)
+public class SecurityConfig {
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(authorizeRequest ->
+                authorizeRequest.antMatchers("/captcha.jpg").permitAll()
+                        .anyRequest().authenticated())
+                .formLogin(from ->
+                        from.failureHandler(new AuthenticationEntryPointFailureHandler(
+                                new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED)
+                        ))
+                                .successHandler((request, response, authentication) -> {
+                                    try (Writer writer = response.getWriter()) {
+                                        writer.write(new ObjectMapper().writeValueAsString(Collections.singletonMap("token", UUID.randomUUID().toString())));
+                                    }
+                                }))
+                .apply(new CaptchaAuthenticationConfigurer())
+                .and()
+                .csrf(AbstractHttpConfigurer::disable);
+
+        return http.build();
+    }
+
+    @Bean
+    UserDetailsService userDetailsService() {
+        UserDetails user = User.withUsername("admin")
+                .password("{noop}password")
+                .roles("ADMIN")
+                .build();
+        return new InMemoryUserDetailsManager(user);
+    }
+}
