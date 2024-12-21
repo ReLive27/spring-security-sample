3404638280f2a3aa02f878c6c8127b5a0b9a4b38
ReLive27 <re_live27@163.com>
2024-12-21 22:10:44 +0800

    Enable webauthn module

---
+++ webauthn/src/main/java/com/relive27/WebAuthnApplication.java
@@ -0,0 +1,16 @@
+package com.relive27;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author: ReLive27
+ * @date: 2024/12/21 22:08
+ */
+@SpringBootApplication
+public class WebAuthnApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebAuthnApplication.class, args);
+    }
+}
