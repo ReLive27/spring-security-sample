0253be3ae1910d193d2b53416c5c1459695939bb
ReLive27 <re_live27@163.com>
2024-12-29 21:32:25 +0800

    Enable jcasbin-authorization module

---
+++ jcasbin-authorization/src/main/java/com/relive27/JcasbinAuthorizationApplication.java
@@ -0,0 +1,16 @@
+package com.relive27;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author: ReLive27
+ * @date: 2024/12/29 21:29
+ */
+@SpringBootApplication
+public class JcasbinAuthorizationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JcasbinAuthorizationApplication.class, args);
+    }
+}
